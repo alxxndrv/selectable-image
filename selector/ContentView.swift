43a89bb132918f424c0a08bdad5c980d4e53e78c
Georgy Alexandrov <goshaa@internet.ru>
2022-04-25 22:20:49 +0300

    image drag&drop

--- selector/ContentView.swift
+++ selector/ContentView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var rects = [(Int, CGRect)]()
+    @State var images = [Image]()
+    @State private var dragOver = false
+    @State var index: Int = 0
     var body: some View {
-        ImageSelectView(image: Image("b"))
+        VStack {
+        if images.isEmpty {
+            VStack {
+                Text("Перетащите изображения сюда")
+                    .fixedSize()
+                    .padding()
+            }.frame(width: 300, height: 300, alignment: .center)
+                .contentShape(Rectangle())
+        } else {
+            ImageSelectView(image: images[index], id: index, rootRects: $rects)
+                .id(index)
+                .onChange(of: rects.count, perform: { _ in
+                    if images.count - 1 == index {
+                        images = []
+                        index = 0
+                    } else {
+                        index += 1
+                    }
+                })
+        }
+    }
+        .onDrop(of: ["public.file-url"], isTargeted: $dragOver) { providers -> Bool in
+            providers.first?.loadDataRepresentation(forTypeIdentifier: "public.file-url", completionHandler: { (data, error) in
+                if let data = data, let path = NSString(data: data, encoding: 4), let url = URL(string: path as String) {
+                    let image = NSImage(contentsOf: url)
+                    DispatchQueue.main.async {
+                        images.append(Image(nsImage: image!))
+                    }
+                }
+            })
+            return true
+        }
     }
 }
 
@@ -18,3 +53,64 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct MyDropDelegate: DropDelegate {
+        @Binding var imageUrls: [Int: URL]
+        @Binding var active: Int
+        
+        func validateDrop(info: DropInfo) -> Bool {
+            return info.hasItemsConforming(to: [.image])
+        }
+        
+        func dropEntered(info: DropInfo) {
+            print("test")
+            NSSound(named: "Morse")?.play()
+        }
+        
+        func performDrop(info: DropInfo) -> Bool {
+            NSSound(named: "Submarine")?.play()
+            
+            let gridPosition = getGridPosition(location: info.location)
+            self.active = gridPosition
+            
+            if let item = info.itemProviders(for: ["public.file-url"]).first {
+                item.loadItem(forTypeIdentifier: "public.file-url", options: nil) { (urlData, error) in
+                    DispatchQueue.main.async {
+                        if let urlData = urlData as? Data {
+                            self.imageUrls[gridPosition] = NSURL(absoluteURLWithDataRepresentation: urlData, relativeTo: nil) as URL
+                        }
+                    }
+                }
+                
+                return true
+                
+            } else {
+                return false
+            }
+
+        }
+        
+        func dropUpdated(info: DropInfo) -> DropProposal? {
+            self.active = getGridPosition(location: info.location)
+                        
+            return nil
+        }
+        
+        func dropExited(info: DropInfo) {
+            self.active = 0
+        }
+        
+        func getGridPosition(location: CGPoint) -> Int {
+            if location.x > 150 && location.y > 150 {
+                return 4
+            } else if location.x > 150 && location.y < 150 {
+                return 3
+            } else if location.x < 150 && location.y > 150 {
+                return 2
+            } else if location.x < 150 && location.y < 150 {
+                return 1
+            } else {
+                return 0
+            }
+        }
+    }
--- selector/ImageSelectView.swift
+++ selector/ImageSelectView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ImageSelectView: View {
     var image: Image
+    var id: Int
     @State var rect: CGRect = CGRect(x: 0, y: 0, width: 0, height: 0)
+    @Binding var rootRects: [(Int, CGRect)]
     var body: some View {
         image
             .resizable()
@@ -21,13 +23,16 @@ struct ImageSelectView: View {
                 rect.origin = value.startLocation
                 rect.size = value.translation
                 print(rect)
+            })
+                    .onEnded({_ in
+                rootRects.append((id, rect))
             }))
-            .overlay(Path(rect).stroke(Color.red, lineWidth: 10))
+            .overlay(Path(rect).stroke(Color.red, lineWidth: 5))
     }
 }
 
 struct ImageSelectView_Previews: PreviewProvider {
     static var previews: some View {
-        ImageSelectView(image: Image("b"))
+        ImageSelectView(image: Image("b"), id: 0, rootRects: .constant([]))
     }
 }
