450bdc5ec0e41bec2d4c82c0458d0896f717dcbb
Georgy Alexandrov <goshaa@internet.ru>
2022-04-25 20:18:52 +0300

    Initial Commit

---
+++ selector/selectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  selectorApp.swift
+//  selector
+//
+//  Created by Georgy Alexandrov on 25.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct selectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
